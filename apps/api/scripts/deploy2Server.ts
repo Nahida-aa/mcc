df4540dcd6e22b0b1d82ea85ff888f32e4b48d78
aa <Nahida-aa@outlook.com>
2026-03-02 03:31:10 +0800

    up

--- apps/api/scripts/deploy2Server.ts
+++ apps/api/scripts/deploy2Server.ts
@@ -7,3 +7,6 @@ const path = '/home/deploy/mcc/current/apps/api'
 
 console.log(chalk.blue(`复制 .env 文件到服务器 ${path}`));
 await $`sshpass -p "${SERVER_PASSWORD}" scp .env ${SERVER_USER}@${SERVER_HOST}:/${path}/.env`
+
+console.log(chalk.blue(`bun install`));
+await $`sshpass -p "${SERVER_PASSWORD}" ssh ${SERVER_USER}@${SERVER_HOST} 'export PATH="/home/deploy/.bun/bin:/home/deploy/.nvm/versions/node/v24.11.1/bin:$PATH" && cd ${path} && bun install && pm2 reload ecosystem.config.js --update-env'`
