52c213fc2c1a9b584ac678884331271795ea922d
aa <Nahida-aa@outlook.com>
2026-03-02 02:07:49 +0800

    up

--- apps/api/ecosystem.config.js
+++ apps/api/ecosystem.config.js
@@ -29,7 +29,7 @@ module.exports = {
       "path": "/home/deploy/mcc",
       "pre-setup": "echo 'commands or local script path to be run on the host before the setup process starts'", // 翻译: 在设置过程开始之前，在主机上运行的命令或本地脚本路径
       "post-setup": "echo 'commands or a script path to be run on the host after cloning the repo'", // 翻译: 在克隆存储库后(设置后)，在主机上运行的命令或脚本路径
-      "pre-deploy": "bun install && pm2:start --env production",
+      "pre-deploy": "ls",
       "post-deploy": "echo 'This is a post-deploy command'",
       "pre-deploy-local": "echo 'This is a local executed command'"
     }
