02a239ecc37b1d0aac1fe84e527c7cc5c5447f8f
aa <Nahida-aa@outlook.com>
2026-03-02 01:50:17 +0800

    up

--- apps/api/ecosystem.config.js
+++ apps/api/ecosystem.config.js
@@ -4,32 +4,36 @@ require('dotenv').config()
 console.log(process.env.SERVER_HOST);
 
 module.exports = {
-  apps : [{
-    name   : "api",
-    script : "src/index.ts",
+  apps: [{
+    name: "api",
+    script: "src/index.ts",
     interpreter: "bun", // Bun interpreter
     env: {
       PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
-       "NODE_ENV": "production",
+      "NODE_ENV": "production",
     },
-            env_production: {
-            PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
-            "NODE_ENV": "production",
-        },
-    kill_timeout : 3000,
-     wait_ready: true,
-     listen_timeout: 10000,
-     log_date_format: 'YYYY-MM-DD HH:mm Z',
+    env_production: {
+      PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
+      "NODE_ENV": "production",
+    },
+    kill_timeout: 3000,
+    wait_ready: true,
+    listen_timeout: 10000,
+    log_date_format: 'YYYY-MM-DD HH:mm Z',
   }],
-    // Deployment Configuration
-  deploy : {
-    production : {
-      "user" : process.env.USER,
-      "host" : process.env.SERVER_HOST,
-      "ref"  : "origin/main",
-      "repo" : "git@github.com:Nahida-aa/mcc.git",
-      "path" : "/home/deploy/mcc",
-      "post-deploy" : "bun install"
+  // Deployment Configuration
+  deploy: {
+    production: {
+      "user": process.env.USER,
+      "host": process.env.SERVER_HOST,
+      "ref": "origin/main",
+      "repo": "git@github.com:Nahida-aa/mcc.git",
+      "path": "/home/deploy/mcc",
+      "pre-setup": "echo 'commands or local script path to be run on the host before the setup process starts'",
+      "post-setup": "echo 'commands or a script path to be run on the host after cloning the repo'",
+      "pre-deploy": "bun install && pm2:start --env production",
+      "post-deploy": "echo 'This is a post-deploy command'",
+      "pre-deploy-local": "echo 'This is a local executed command'"
     }
   }
 }
