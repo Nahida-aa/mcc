ed6e7bd34edae1d8894074d8f01fdc09d3e3d182
aa <Nahida-aa@outlook.com>
2026-03-02 11:59:42 +0800

    up

--- apps/api/scripts/deploy.ts
+++ apps/api/scripts/deploy.ts
@@ -1,13 +1,13 @@
 import { $, chalk, echo } from "zx";
 
 console.log(chalk.blue(`git pull`))
-const p1 = $`git pull`.stdio("inherit");
+const p1 = $`git pull`;
 for await (const chunk of p1.stdout) {
   echo(chunk)
 }
 
 console.log(chalk.blue(`bun i --filter apps/api --production`));
-const p2 = $`bun i --filter apps/api --production`.stdio("inherit");
+const p2 = $`bun i --filter apps/api --production`;
 for await (const chunk of p2.stdout) {
   echo(chunk)
 }
